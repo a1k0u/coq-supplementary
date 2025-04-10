8e319957e319cb038c8531edad71bf561e509723
a1k0u <alko7enko@gmail.com>
2025-04-10 14:55:22 +0300

    feat: add Id.v

--- src/Id.v
+++ src/Id.v
@@ -2,6 +2,7 @@
 
 Require Import Arith Arith.EqNat.
 Require Import Lia.
+Require Import Nat.
 
 Inductive id : Type :=
   Id : nat -> id.
@@ -54,6 +55,14 @@ Ltac prove_with th :=
     end;
   try (constructor; assumption); congruence.
 
+Lemma not_eq_id_sym (x y : id) : x <> y -> y <> x.
+Proof.
+  intro H. 
+  destruct x. destruct y. 
+  apply not_eq_sym in H. 
+  apply H.
+Qed.
+
 Lemma lt_eq_lt_id_dec: forall (id1 id2 : id), {id1 i< id2} + {id1 = id2} + {id2 i< id1}.
 Proof. prove_with lt_eq_lt_dec. Qed.
   
@@ -64,30 +73,86 @@ Lemma le_gt_id_dec : forall id1 id2 : id, {id1 i<= id2} + {id1 i> id2}.
 Proof. prove_with le_gt_dec. Qed.
 
 Lemma id_eq_dec : forall id1 id2 : id, {id1 = id2} + {id1 <> id2}.
-Proof. admit. Admitted.
+Proof. 
+  intros id1 id2. 
+  destruct id1 as [n1], id2 as [n2].
+  destruct (eq_nat_dec n1 n2) as [Heq | Hneq].
+  - left. rewrite Heq. reflexivity.
+  - right. injection. intro H'. contradiction.
+Qed.
 
 Lemma eq_id : forall (T:Type) x (p q:T), (if id_eq_dec x x then p else q) = p.
-Proof. admit. Admitted.
+Proof. 
+  intros t x p q.
+  destruct (id_eq_dec x x) as [Heq | Hneq].
+  - reflexivity.
+  - contradiction.
+Qed.
 
 Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if id_eq_dec x y then p else q) = q.
-Proof. admit. Admitted.
+Proof. 
+  intros t x y p q H.
+  destruct (id_eq_dec x y) as [Heq | Hneq].
+  - contradiction.
+  - reflexivity.
+Qed.
 
 Lemma lt_gt_id_false : forall id1 id2 : id,
     id1 i> id2 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof. 
+  intros id1 id2 H1 H2.
+  destruct id1 as [n0], id2 as [m0].
+  inversion H1 as [n1 m1 Hi1].
+  inversion H2 as [n2 m2 Hi2].
+  subst.
+  apply (Nat.lt_asymm m0 n0).
+  - exact Hi1.
+  - exact Hi2.
+Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id,
     id2 i<= id1 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2 H1 H2.
+  destruct id1 as [n0], id2 as [m0].
+  inversion H1 as [n1 m1 Hi1].
+  inversion H2 as [n2 m2 Hi2].
+  subst.
+  apply (Nat.lt_nge n0 m0).
+  - exact Hi2.
+  - exact Hi1. 
+Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, 
     id1 i<= id2 -> {id1 = id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof. 
+  intros id1 id2 H1.
+  destruct id1 as [n1], id2 as [m1].
+  destruct (le_lt_eq_dec n1 m1) as [e1 | e1].
+  inversion H1 as [n2 m2 H2].
+  subst.
+  - exact H2.
+  - right. apply gt_conv. assumption.
+  - left. rewrite e1. reflexivity.
+Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id,
     id1 <> id2 -> {id1 i> id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2 H.
+  destruct (gt_eq_gt_id_dec id1 id2) as [Z1 | Z2].
+  - destruct Z1 as [l | r]. 
+    + left. apply l.
+    + right. contradiction.
+  - right. apply Z2.
+  Qed.
     
 Lemma eq_gt_id_false : forall id1 id2 : id,
     id1 = id2 -> id1 i> id2 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2 H1 H2.
+  destruct id1 as [n1], id2 as [m1].
+  inversion H1. inversion H2. subst.
+  apply (Nat.lt_irrefl m1).
+  assumption.
+Qed.
